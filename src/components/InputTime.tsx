dad4d1dde31c1c9bbf80c8d40591fec149c40e4f
Hiago <hiagoisoppotrajano@gmail.com>
2023-07-16 01:22:09 -0300

    refatorando

--- src/components/InputTime.tsx
+++ src/components/InputTime.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { StyledInput, NumbersDiv, StyledDots } from "../assets/StyledComponents";
 
 type InputProps = {
